a66f14d01e54e4048d58073f067716513edf0c39
08kael08 <worldcg08@gmail.com>
2022-09-08 23:06:46 +0300

    Roman to Integer

--- Leet/0003_roman_to_integer/main.go
+++ Leet/0003_roman_to_integer/main.go
@@ -1,13 +1,38 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	fmt.Println(romanToInt("IX"))
 }
 
 func romanToInt(s string) int {
-	m := map[string]int{"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+	roman := map[string]int{"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
 
-	for i, rune := range s {
+	romanSubst := map[string]int{"IV": 4, "IX": 9, "XL": 40, "XC": 90, "CD": 400, "CM": 900}
+
+	var prevChar string
+	var subNumber int
+	var output int
+
+	for _, rune := range s {
+		char := string(rune)
+		if prevChar != "" {
+			subNumber = romanSubst[prevChar+char]
+
+			if subNumber != 0 {
+				output -= roman[prevChar]
+				output += subNumber
+				continue
+			}
+		}
+
+		prevChar = char
+		output += roman[char]
 
 	}
+
+	return output
 }
